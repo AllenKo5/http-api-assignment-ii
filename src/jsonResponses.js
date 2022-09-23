220a9c2b9de5591fbdb246c99c3f84561926a81e
AllenKo5 <69921088+AllenKo5@users.noreply.github.com>
2022-09-22 20:41:58 -0400

    finished setting up server and responses

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,3 +1,6 @@
+const users = {};
+
+// helper functions
 const respondJSON = (request, response, status, object) => {
   response.writeHead(status, { 'Content-Type': 'application/json' });
   response.write(JSON.stringify(object));
@@ -9,22 +12,60 @@ const respondJSONMeta = (request, response, status) => {
   response.end();
 };
 
+// handler functions
 const getUsers = (request, response) => {
+  const responseJSON = {
+    users,
+  };
 
+  return respondJSON(request, response, 200, responseJSON);
 };
 
-const getUsersMeta = (request, response) => {
+const getUsersMeta = (request, response) => respondJSONMeta(request, response, 200);
 
-};
+const addUser = (request, response, body) => {
+  const responseJSON = {
+    message: 'Name and age are both required.',
+  };
 
-const notReal = (request, response) => {
+  if (!body.name || !body.age) {
+    responseJSON.id = 'addUserMissingParams';
+    return respondJSON(request, response, 400, responseJSON);
+  }
 
-};
+  let statusCode = 204;
 
-const notRealMeta = (request, response) => {
+  if (!users[body.name]) {
+    statusCode = 201;
+    users[body.name] = {};
+  }
 
+  users[body.name].name = body.name;
+  users[body.name].age = body.age;
+
+  if (statusCode === 201) {
+    responseJSON.message = 'Created successfully.';
+    return respondJSON(request, response, statusCode, responseJSON);
+  }
+
+  return respondJSONMeta(request, response, statusCode);
 };
 
 const notFound = (request, response) => {
+  const responseJSON = {
+    message: 'The page you are looking for was not found.',
+    id: 'notFound',
+  };
 
+  return respondJSON(request, response, 404, responseJSON);
+};
+
+const notFoundMeta = (request, response) => respondJSONMeta(request, response, 404);
+
+module.exports = {
+  getUsers,
+  getUsersMeta,
+  addUser,
+  notFound,
+  notFoundMeta,
 };
